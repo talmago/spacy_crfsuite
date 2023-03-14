c4538e94c2accc3131f0b339de5da0c7e40d71eb
Tal Almagor <almagoric@gmail.com>
2023-03-14 11:58:33 +0200

    bump version to v1.6.0

--- spacy_crfsuite/about.py
+++ spacy_crfsuite/about.py
@@ -1,5 +1,5 @@
 __title__ = "spacy_crfsuite"
-__version__ = "1.5.0"
+__version__ = "1.6.0"
 __summary__ = "spaCy pipeline component for CRF entity extraction"
 __author__ = "Tal Almagor"
 __email__ = "almagoric@gmail.com"
